4542e5683af80b4e8755a379a6d966c9f5f14f89
KPrasch <kieranprasch@gmail.com>
2014-09-15 22:49:20 -0400

    Finishing up the first models.

--- dispatch_gmail/models.py
+++ dispatch_gmail/models.py
@@ -2,13 +2,20 @@ from django.db import models
 
 # Create your models here.
 
+class Email(models.Model):
+    sender = models.EmailField(max_length=256)
+    subject = models.CharField(max_length=100)
+    body = models.TextField()
+
+
 class Dispatch(models.Model):
-    unit =
-    venue =
+    source = models.ForeignKey(email)
+    unit = models.CharField(max_length=20)
+    venue = models.CharField(max_length=50)
     incident = models.CharField(max_length=600)
-    location =
-    interscection =
+    location = models.CharField(max_length=200)
+    interscection = models.CharField(max_length=50)
     dispatch_time = models.DateTimeField('Date and Time of Initial Dispatch')
-    nature =
-    common =
-    additional =
+    nature = models.CharField(max_length=100)
+    common = models.CharField(max_length=200)
+    additional = models.CharField(max_length=200)
