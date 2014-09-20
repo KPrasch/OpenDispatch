d82ae681eb06251fe8b7e615ac959079e670818b
KPrasch <kieranprasch@gmail.com>
2014-09-19 20:56:45 -0400

    Fixed syntax and Indent. Returning HttpResponse.

--- dispatch_gmail/views.py
+++ dispatch_gmail/views.py
@@ -18,46 +18,49 @@ def extract_gmail_incidents(payload):
     else:
         return '\n'.join([extract_gmail_incidents(part.get_payload()) for part in payload])
 
-# Connect and Authenticate Gmail account.  This is happening in the Terminal for now.
-# Need Frontend / Gmail API Integration here
-usernm = raw_input("Username:")
-passwd = getpass.getpass(prompt='Password: ', stream=None)
-conn = imaplib.IMAP4_SSL("imap.gmail.com", 993)
-conn.login(usernm,passwd)
+    # Connect and Authenticate Gmail account.  This is happening in the Terminal for now.
+    # Need Frontend / Gmail API Integration here
+    usernm = raw_input("Username:")
+    passwd = getpass.getpass(prompt='Password: ', stream=None)
+    conn = imaplib.IMAP4_SSL("imap.gmail.com", 993)
+    conn.login(usernm,passwd)
 
-# Selecting by Gmail Label 'Dispatch' like It's an IMAP folder.
-print("Total Incidents Found:", conn.select('Dispatch'))
-# Only trying to parse the emails that are relevant. Selecting by sender and subject.
-typ, data = conn.search(None, '(FROM "messaging@iamresponding.com" SUBJECT "Company 43")')
+    # Selecting by Gmail Label 'Dispatch' like It's an IMAP folder.
+    print("Total Incidents Found:", conn.select('Dispatch'))
+    # Only trying to parse the emails that are relevant. Selecting by sender and subject.
+    typ, data = conn.search(None, '(FROM "messaging@iamresponding.com" SUBJECT "Company 43")')
 
-# Create a set of target strings, and craete a regular expressions pattern to select the text between them.
-keys = set(('Inc', 'Nature', 'XSts', 'Common', 'Addtl', 'Loc', 'Date', 'Time'))
-key_re = re.compile('(' + '|'.join(re.escape(key) for key in keys) + '):', re.IGNORECASE)
+    # Create a set of target strings, and craete a regular expressions pattern to select the text between them.
+    keys = set(('Inc', 'Nature', 'XSts', 'Common', 'Addtl', 'Loc', 'Date', 'Time'))
+    key_re = re.compile('(' + '|'.join(re.escape(key) for key in keys) + '):', re.IGNORECASE)
 
-try:
-    for num in data[0].split():
-        typ, msg_data = conn.fetch(num, '(RFC822)')
-        for response_part in msg_data:
-            if isinstance(response_part,tuple):
-                msg = email.message_from_string(response_part[1])
-                payload = msg.get_payload()
-                body = extract_gmail_incidents(payload)
-                try:
-                  # Extract the incident text data with regular expressions and create a dictionary for each incident.
-                  key_locations = key_re.split(body)[1:]
-                  incident_dict = {k: v.strip() for k,v in zip(key_locations[::2], key_locations[1::2])}
-                  # Create a model instance for each incident.
-                  incident = Incident.objects.create(**incident_dict)
-                  # Save the Incident to the database.
-                  #incident.save()
-                  print "Successfuly created incident # %s:%s." % (incident.id, incident.Inc)
-                except IndexError:
-                  print "malformed incident email."
-
-#close the connection and logout.
-finally:
     try:
-        conn.close()
-    except:
-        pass
+        for num in data[0].split():
+            typ, msg_data = conn.fetch(num, '(RFC822)')
+            for response_part in msg_data:
+                if isinstance(response_part,tuple):
+
+                    msg = email.message_from_string(response_part[1])
+                    payload = msg.get_payload()
+                    body = extract_gmail_incidents(payload)
+
+                    try:
+                      # Extract the incident text data with regular expressions and create a dictionary for each incident.
+                      key_locations = key_re.split(body)[1:]
+                      incident_dict = {k: v.strip() for k,v in zip(key_locations[::2], key_locations[1::2])}
+                      # Create a model instance for each incident.
+                      incident = Incident.objects.create(**incident_dict)
+                      # Save the Incident to the database.
+                      incident.save()
+                      print "Successfuly created incident # %s:%s." % (incident.id, incident.Inc)
+                    except IndexError:
+                      print "Malformed incident email found.  Incident #%s" % (incident.id)
+
+    # Close the connection and logout.
+    finally:
+        try:
+            conn.close()
+        except:
+            pass
     conn.logout()
+    return HttpResponse('Finished retrieving incident messages.')
