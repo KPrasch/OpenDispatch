617ae7750f342fdb9d24c73e8dd296151cd62a99
KPrasch <kieranprasch@gmail.com>
2015-02-12 18:30:21 -0800

    Decoupled agency specific settings, and created a master view for incident importing.

--- dispatch/views.py
+++ dispatch/views.py
@@ -14,25 +14,65 @@ from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 import json as simplejson
 import simplejson
+import dispatch_settings
 
 
-#Needs refactoring to accept any key, and use that key....User configurable might be the answer. 
+def import_incidents(source):
+    '''
+    '''
+    if source == 'twitter':
+      raw_incident = get_twitter_incidents(dispatch_settings.TWITTER_USERNAME)
+    elif source == 'email':
+      raw_incident = import_email_incidents(dispatch_settings.EMAIL_USERNAME)
+    else:
+      return ValueError("Invalid dispatch source.  Please specify twitter, email or cad.")  
+      
+    normalized_data = normalize_incidnt_data(raw_incident.payload)
+    parse = parse_incident(normalized_data.payload, raw_incident.recieved_datetime)
+    incident_dict = parse.incident_dict
+    geocode = geocode_incident(incident_dict)
+    incident = UlsterIncident.objects.create(datetime = raw_incident.recieved_datetime, lat = geocode[0], long = geocode[1], **incident_dict)
+    incident.save()
+    print "Created Incident %d" % incident.id
+
+    return HttpResponse('200')
+
+def get_current_weather(request):
+    pass
+
+def get_historical_weather(request):
+    pass
+
+def normalize_incidnt_data(payload):
+    '''
+    Converts unicode data to a python string.
+    '''
+    decoded_payload =  unicodedata.normalize('NFKD', payload).encode('ascii', 'ignore') 
+    payload = decoded_payload.replace('\n', '').replace('\r', '')
+    return payload
+    
 def parse_incident(payload, sent):
-    # Create a set of target strings, and craete a regular expressions pattern to select the text between them.
-    keys = set(('Unit', 'Venue', 'Inc', 'Nature', 'XSts', 'Common', 'Addtl', 'Loc', 'Date', 'Time'))
-    key_re = re.compile('(' + '|'.join(re.escape(key) for key in keys) + '):', re.IGNORECASE)
+    '''
+    Using configurable incident data fields, parse the data into incident models with all of the information required.
+    '''
+    keys = set(dispatch_settings.INCIDENT_FIELDS)
+    key_re = re.compile('(' + '|'.join(re.escape(key) for key in keys) + ')%r' % dispatch_settings.DELINIATER, re.IGNORECASE)
     key_locations = key_re.split(payload)[1:]
     incident_dict = {k: v.strip() for k,v in zip(key_locations[::2], key_locations[1::2])}
-    location_fields = ['XSts', 'Loc', 'Venue'] 
-    incident_location_data = [incident_dict[x] for x in location_fields]
-    get_loc = get_coordinates(" ".join(incident_location_data))
     # Create a model instance for each incident.
-    incident = UlsterIncident.objects.create(payload = payload, datetime = sent, lat = get_loc[0], long = get_loc[1], **incident_dict)
-    print "Created incident %s" % incident.id
-    return incident
+    return incident_dict
 
-def get_coordinates(incident_location_data, from_sensor=False):
+def get_coordinates(incident_dict, from_sensor=False):
+    '''
+    Uses the unauthenticated Google Maps API V3.  using passed incident dictionary,
+    gather relevant location text and return a latitude and logitude for an incident. 
+    '''
     googleGeocodeUrl = 'http://maps.googleapis.com/maps/api/geocode/json?'
+    location_fields = dispatch_settings.LOCATION_FIELDS
+    incident_location_data = [incident_dict[x] for x in location_fields]
+    incident_location_data = " ".join(incident_location_data).LOWER  + dispatch_settings.LOCALE_STATE
+    
+    #Back to unicode
     incident_location_data = incident_location_data.encode('utf-8')
     params = {
         'address': incident_location_data,
--- dispatch_twitter/views.py
+++ dispatch_twitter/views.py
@@ -7,7 +7,7 @@ import unicodedata
 from TwitterAPI import TwitterAPI
 from chartit import DataPool, Chart
 from dispatch.models import UlsterIncident
-from dispatch.views import parse_incident
+from dispatch.views import *
 from dispatch_gmail.models import IncidentEmail
 from dispatch_twitter.models import TwitterIncident
 from django.http import HttpResponse
@@ -15,25 +15,44 @@ from django.shortcuts import render, redirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 import json as simplejson
-from private import local-settings
+from private import local_settings
 
-def import_twitter_incidents(request):
+
+def get_twitter_incidents(twitter_username):
     '''
+    Creates incidents from a twitter feed -
+    given that the status of each status is a sting in dictionary format
+    containing the details of a 911 incident.
+    
+    Using application only authentication from the private module,
+    attempt to retrieve as many statuses from one public twitter user as possible,
+    using the public Twitter REST API.
+    
+    Then create a datetime object, save the raw data, normalize it, parse it, and geocode it
+    from the unauthenticated Google Maps API V3.
+    
+    Save each incident to the database.
     '''
     api = TwitterAPI(settings.TWITTER_TOKEN_1, settings.TWITTER_TOKEN_2, settings.TWITTER_TOKEN_3, settings.TWITTER_TOKEN_4)
-    r = api.request('statuses/user_timeline', {'screen_name':'ulstercounty911', \
+    r = api.request('statuses/user_timeline', {'screen_name':'%s' % twitter_username, \
+                                               #We don't want anything except the statuses.
                                                'exclude_replies':'true', \
+                                               # 3,200 is the TwitterAPI rate limit.
                                                'count': '3200', \
+                                               #Further reduce our response size by excluding the user's metadata.
+                                               'trim_user': 'true', \
+                                               #Further reduce our response size by excluding other details
+                                               'contributor_details': 'false', \
+                                               #The ID of the oldest status to start importing from. 
+                                               #This defaults to the oldest possible if the maximum rate limit is reached.
+                                               'since_id': '' \
                                                })
     
     for status in r:
         received_datetime = datetime.strptime(status["created_at"], "%a %b %d %H:%M:%S +0000 %Y")
-        normalized_data =  unicodedata.normalize('NFKD', status["text"]).encode('ascii', 'ignore') 
-        payload = normalized_data.replace('\n', '').replace('\r', '')
-        twitter_incident = TwitterIncident.objects.create(datetime = received_datetime, payload = payload)
-        twitter_incident.save()
-        sys.stdout.write("Saved twitter dispatch %s \r" % twitter_incident.id)
+        payload = status["text"]
+        raw_incident = RawIncident.objects.create(datetime = received_datetime, payload = payload)
+        raw_incident.save()
+        sys.stdout.write("Saved raw unicode twitter dispatch %s \r" % raw_incident.id)
         sys.stdout.flush()
-        parse_incident(payload, received_datetime)
-    print "Done."
-    return render(request, 'dashboard.html')
+    return payload, recieved_datetime
