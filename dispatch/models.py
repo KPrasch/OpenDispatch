a4d74f39e4f791d5afefeab9a9fa730970e36363
KPrasch <kieranprasch@gmail.com>
2015-02-13 21:55:32 -0800

    model methods for Incident

--- dispatch/models.py
+++ dispatch/models.py
@@ -18,9 +18,35 @@ class Incident(models.Model):
     created_time = models.DateTimeField(auto_add_now=True)
     #weather_status = IntegerField()
     
-    def get_raw(self):
+    def raw(self):
         return self.payload
-
+    
+    def geoloc(self):
+        geo_loc = " ".join(self.lat, self.lng)
+        return geo_loc
+   
+    def recieved_delay(self):
+        if self.dispatch_time != self.recieved_time:
+           delay = (self.dispatch_time-self.recieved_time).total_seconds()
+           return delay
+        else:
+           return None
+       
+    def created_delay(self):
+         if self.recieved_time != self.created_time:
+           delay = (self.recieved_time-selfself.created_time).total_seconds()
+           return delay
+         else:
+           return None
+       
+    def time_disparity(self):
+        rd = self.recieved_delay(self)
+        cd = self.created_delay(self)
+        if rd or cd is not None:
+            return True
+        else:
+            return False
+               
     class Meta:
       unique_together = ["payload", "datetime"]
       order_by = ["datetime"]
@@ -29,6 +55,10 @@ class IncidentData(models.Model):
     incident = models.ForeignKey(Incident)
     key = models.CharField(max_length=200, blank=True)
     value = models.CharField(max_length=200, blank=True)
+    
+    def incident(self):
+        return self.incident.id
+        
     
     
 
