56b95e868d0d06fa3fa2ff7a236cbb9b9f654609
Zack Kollar <rallokkcaz@gmail.com>
2016-03-01 02:20:53 -0500

    Lots of form work, CSS and JS to work modal views. No AJAX for the forms yet, too late at night.

--- static-files/global/js/auth.js
+++ static-files/global/js/auth.js
@@ -1,3 +1,11 @@
+active = "add-location-form";
+$(document).ready(function() {
+    $("#add-location-form-container").modal({
+      escapeClose: true,
+      clickClose: true,
+      showClose: false
+    });
+});
 $(function() {
     $("#login-form").on("submit", function(event) {
         preventDefault();
@@ -5,5 +13,25 @@ $(function() {
             type: "POST",
 
         });
+    });
+
+    $("#add-location-form-progress > a").click(function(event) {
+        if (active == event.target.id) {
+            return;
+        }
+
+        wasClicked = "#add-"+event.target.id.split("-")[1]+"-form";
+        removeActive = "#show-"+active.split("-")[1]+"-form";
+
+        console.log(wasClicked);
+        console.log(removeActive);
+
+        $("#"+active).css('display', 'none').promise().done(function() {
+            $(removeActive).removeClass('active');
+            $(wasClicked).show();
+            $("#"+event.target.id).addClass('active');
+        });
+
+        active = wasClicked.replace('#', '');
     });
 });
