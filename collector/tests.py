83f8e45a4ac93e9c2990d08eebb4a5628712003c
jMyles <justin@justinholmes.com>
2016-03-14 20:55:04 -0400

    collector tests now comply with new factory and stream.

--- collector/tests.py
+++ collector/tests.py
@@ -8,32 +8,31 @@ from apps.collect.views import process_import
 from collector.bulletin import BulletinTweet
 from collector.client import store_stream
 from collector.object_store_backend import COLLECTOR_BACKEND
-from collector.test_utils import FakeBulletin
+from collector.test_utils import FakeBulletinFactory, FakeBulletinStream
 
 
 class TwitterAPITestCase(TestCase):
 
     def setUp(self):
-        self.fake_bulletin = FakeBulletin('good').twitter()
+        self.fake_bulletin = FakeBulletinFactory(BulletinTweet, 'good')
         super(TwitterAPITestCase, self).setUp()
 
     def test_incidentless_tweet_raises_geocoding_error(self):
-        self.fake_bulletin = FakeBulletin('bad').twitter()
-        mock_tweet = self.fake_bulletin.iter_lines().next()
-        tweet_bulletin = BulletinTweet(mock_tweet)
+        fbf = FakeBulletinFactory(BulletinTweet, 'bad')
+        tweet_bulletin = fbf.next()
         self.assertRaises(ValueError, process_import, tweet_bulletin)  # TODO: Decide if we want to test process_import at this stage.
 
     def test_tweet_is_pushed_to_backend(self):
-        fts = FakeBulletin('good').twitter()
-        store_stream(reactor, fts)
+        fbs = FakeBulletinStream('good')
+        store_stream(reactor, fbs)
         tweet = COLLECTOR_BACKEND.pop()
-        self.assertEqual(tweet, fts.last_stream_item)
+        self.assertEqual(tweet, fbs.last_stream_item)
 
     def test_tweet_is_preprocessed(self):
-        self.fake_bulletin = FakeBulletin('good').twitter()
-        mock_tweet = self.fake_bulletin.iter_lines().next()
+        fbs = FakeBulletinStream('good')
+        mock_tweet = fbs.iter_lines().next()
         tweet_bulletin = BulletinTweet(mock_tweet)
-        event = json.loads(self.fake_bulletin.last_stream_item)
+        event = json.loads(fbs.last_stream_item)
         self.assertEqual(event['text'], tweet_bulletin.payload)
         self.assertIsInstance(tweet_bulletin.received_dt, datetime.datetime)
 
